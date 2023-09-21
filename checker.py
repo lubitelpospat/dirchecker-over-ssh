0f8d2e02e3302b6fe3bcf961abd031dbf3d9524e
Alex <sizykh.a.d@gmail.com>
2023-09-21 11:20:24 -0600

    fixed wrong error message in the beginning of output

--- checker.py
+++ checker.py
@@ -144,7 +144,10 @@ with  fabric.Connection(host=args.REMOTE_HOST, connect_kwargs={"password": PASSW
     localdirs = list(filter(lambda x: not (x in dirs_to_ignore), map(lambda x: x.name,pathlib.Path(args.BASE_DIR).glob(args.pattern)))) #directories on local host in REMOTE_HOST
     localdirs_present_on_remote_server = set(localdirs).intersection(set(dirnames))
     localdirs_not_found_on_remote_server = set(localdirs).difference(set(dirnames))
-    print(Fore.RED + f"{len(localdirs_not_found_on_remote_server)} folder(s) not found on remote server: {','.join(localdirs_not_found_on_remote_server)}")
+    if len(localdirs_not_found_on_remote_server) >0:
+
+        print(Fore.RED + f"{len(localdirs_not_found_on_remote_server)} folder(s) not found on remote server: {','.join(localdirs_not_found_on_remote_server)}, exiting now", file=sys.stderr)
+        sys.exit(1)
    
 
     if len(localdirs_present_on_remote_server) == 0:
